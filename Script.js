8b5cafb172a536c7b1b94dee00aff6ade196efe6
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2021-09-16 15:22:39 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -18,36 +18,52 @@ const _environment = _customData._environment;
 //your code here....
 
 
-//Template for Environment, Wall,(more in the future):
+//Template for Environment enhancement, Wall, Custom event:
 
 //wall:
-_obstacles.push(
-    {
-        "_time": Beat,
-        "_lineIndex": 0,
-        "_type": 1,
-        "_duration": When animating in custom data,
-        "_width": 0,
-        "_customData": {
-            "_track": ["yourTrack1","yourTrack2"..... you can make one or more tracks],
-            "_scale": [x, y, z],
-            "_position": [x, y, z],
-            "_rotation":[x, y, z]
-            //more customData here
-        }
-    }
-);
+//_obstacles.push(
+//    {
+//        "_time": Beat,
+//        "_lineIndex": 0,
+//        "_type": 1,
+//        "_duration": When animating in custom data,
+//        "_width": 0,
+//        "_customData": {
+//            "_track": ["yourTrack1","yourTrack2"..... you can make one or more tracks],
+//            "_scale": [x, y, z],
+//            "_position": [x, y, z],
+//            "_rotation":[x, y, z]
+//            //more customData here
+//        }
+//    }
+//);
+
+
 //Environment:
-_environment.push(
-    {
-        "_id": "Find in Chroma logs",
-        "_lookupMethod": "Regex, Exact, Contains",
-        "_scale": [x, y, z],
-        "_position": [x, y, z],
-        "_rotation":[x, y, z]
-        //more customData here
-    }
-);
+//_environment.push(
+//    {
+//        "_id": "Find in Chroma logs",
+//        "_lookupMethod": "Regex, Exact, Contains",
+//        "_scale": [x, y, z],
+//       "_position": [x, y, z],
+//        "_rotation":[x, y, z]
+//        //more customData here
+//    }
+//);
+
+
+//custom event:
+//_customEvents.push(
+//    {
+//        "_time" : 66,
+//      "_type" : "AnimateTrack",
+//        "_data" : {
+//          "_rotation" : [[0,0,0,0],[0,50000,0,1]],
+//          "_duration" : 100,
+//          "_track" : "beginingwall"
+//        }
+//    }
+//);
 
 
 fs.writeFileSync(OUTPUT, JSON.stringify(difficulty, null, 0));
